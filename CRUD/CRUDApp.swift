6d32a50ab9761485518c2211cb962720a764b728
andycheng <andychz0519@gmail.com>
2020-11-25 10:24:21 +0800

    Initial Commit

---
+++ CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by User12 on 2020/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
